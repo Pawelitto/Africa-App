7d4ceeab672b1db7822c3ef138b4914966b9c61f
Paweł Pietras <pawel.pietras144@gmail.com>
2024-01-27 12:37:23 +0100

    5. Let’s develop the detail view for each animal with dynamic data - Part 2

---
+++ Africa/View/ExternalWeblinkView.swift
@@ -0,0 +1,18 @@
+//
+//  ExternalWeblinkView.swift
+//  Africa
+//
+//  Created by Paweł Pietras on 27/01/2024.
+//
+
+import SwiftUI
+
+struct ExternalWeblinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExternalWeblinkView()
+}
